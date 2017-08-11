b0c4dce5982a426d0fc87116fc96445cd21b056d
Bryan Fillmer <bryan.fillmer@gmail.com>
2017-08-10 18:25:09 -0600

    Some json.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = () => 'Hello world'
+
+const fetch = require('node-fetch')
+
+module.exports = async function (req, res) {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+  const json = await response.json()
+
+  return json.example
+}
